3a139ea65a2935382a31e93c3eab8921eddd5d63
kcorkan <kcorkan@rallydev.com>
2014-12-09 17:33:15 -0700

    updates to add granularity and state selectors, export functionality, and series

--- anystate-cycle-time/src/javascript/_ts-calculator.js
+++ anystate-cycle-time/src/javascript/_ts-calculator.js
@@ -1,14 +1,11 @@
 
 Ext.define('AnystateCycleCalculator', {
     extend: 'Rally.data.lookback.calculator.BaseCalculator',
-    fromState: "In-Progress",
-    toState:"Accepted",
     groupField: 'ScheduleState',
     
     runCalculation: function(snapshots) {
-        var final_state = this.toState;
-        var initial_state = this.fromState;
-        console.log('blah',initial_state,final_state,snapshots);
+        var final_state = this.endState;
+        var initial_state = this.startState;
         
         /* iterate over the snapshots (each of which represents a transition
          * 1.  Save the ones that are transitioning INTO the initial state in a hash by object id so that
@@ -28,141 +25,195 @@ Ext.define('AnystateCycleCalculator', {
             var state = snapshot[this.groupField];
             
             if (dates_by_oid[snapshot.ObjectID] == undefined){
-                dates_by_oid[snapshot.ObjectID] = {_finalDate: null, _startDate: null};
+                var object_type = snapshot._TypeHierarchy.slice(-1)[0];
+                dates_by_oid[snapshot.ObjectID] = {_finalDate: null, _startDate: null, _type: object_type};
             }
-            if (state == final_state && snapshot._ValidFrom && snapshot._PreviousValues 
-                    && snapshot._PreviousValues.ScheduleState != undefined){
+            if (state == final_state && snapshot._ValidFrom && ((snapshot._PreviousValues 
+                    && snapshot._PreviousValues.ScheduleState != undefined) ||  (snapshot._SnapshotNumber == 0))){
+
                 dates_by_oid[snapshot.ObjectID]._finalDate = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);  
-                console.log('final',snapshot.ObjectID, dates_by_oid[snapshot.ObjectID]._finalDate);
+
                 if (Rally.util.DateTime.getDifference(dates_by_oid[snapshot.ObjectID]._finalDate,end_date,'day') > 0){
                     end_date = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);  
-                    console.log(end_date, snapshot.ObjectID);
+
                 } 
             }
-            console.log('snapshot',snapshot);
-            if (state == initial_state && snapshot._ValidFrom && snapshot._PreviousValues 
-                    && snapshot._PreviousValues.ScheduleState != undefined){
-                dates_by_oid[snapshot.ObjectID]._startDate = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);
+
+            if (state == initial_state && snapshot._ValidFrom && ((snapshot._PreviousValues 
+                    && snapshot._PreviousValues.ScheduleState != undefined) || (snapshot._SnapshotNumber == 0))){ //0 means that this is the first snapshot
+                var diff = 0; 
+                if (dates_by_oid[snapshot.ObjectID]._startDate){
+                    var diff = Rally.util.DateTime.getDifference(new Date(dates_by_oid[snapshot.ObjectID]._startDate), new Date(snapshot._ValidFrom),'day');
+                }
+                if (diff >= 0){  //we want the earliest date this transitioned into the start state
+                    dates_by_oid[snapshot.ObjectID]._startDate = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);
+                }
+
                 if (Rally.util.DateTime.getDifference(dates_by_oid[snapshot.ObjectID]._startDate,start_date,'day') < 0){
                     start_date = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);  
                 } 
             }
          }, this);
-         console.log('dates_by_oid',dates_by_oid);
-         console.log('start,end dates',start_date,end_date);
          
-         var categories = this._getCategories(start_date, end_date, dates_by_oid);
-         console.log('categories', categories);
-         
-         var series = this._getSeries(categories, dates_by_oid);  
-         console.log('series',series);
+         var categories = this._getCategories(start_date, end_date, dates_by_oid, this.granularity);
+   //      console.log('categories', categories);
+
+         var series = [];
+         series.push(this._getSeries(categories, dates_by_oid, this.granularity));  
+         series.push(this._getSeries(categories, dates_by_oid, this.granularity,'HierarchicalRequirement'));  
+         series.push(this._getSeries(categories, dates_by_oid, this.granularity,'Defect'));  
+ //        console.log('series',series);
          
         return {
             series: series,
             categories: categories
         }
     },
-    _getDateKey: function(d){
+    _getDateKey: function(d, granularity){
+        switch(granularity){
+            case 'Week':
+                return 'Week' + Rally.util.DateTime.format(d, 'W y');
+            case 'Month':
+                return Rally.util.DateTime.format(d, 'M y');
+            default:
+        }
         return Rally.util.DateTime.format(d, 'Y-m-d');  
     },
-    _getCategories: function(start_date, end_date, dates_by_oid){
+    
+    _getCategories: function(start_date, end_date, dates_by_oid, granularity){
         var categories = [];
-        var i = 0;
         for (var d=start_date; d < end_date; d = Rally.util.DateTime.add(d,"day",1)){
-            categories[i] = this._getDateKey(d);  
-            i++;
+            var date_key = this._getDateKey(d, granularity);  
+            if (!Ext.Array.contains(categories, date_key)){
+                categories.push(date_key);
+            }
         }
         return categories;  
     },
-    _getSeries: function(categories, dates_by_oid){
+    
+    _getSeries: function(categories, dates_by_oid, granularity, type){
         var stats_by_date = {}; 
         
         Ext.Object.each(dates_by_oid, function(oid, dates){
-            if (dates._startDate && dates._finalDate) {
+            if (dates._startDate && dates._finalDate && (type == undefined || dates._type == type )) {
                 var diff = Rally.util.DateTime.getDifference(dates._finalDate, dates._startDate,'day');
                 dates_by_oid[oid]._cycleTime = diff;
-                var fd_key = this._getDateKey(dates._finalDate);
+                var fd_key = this._getDateKey(dates._finalDate, granularity);
                
-                if (stats_by_date[fd_key] == undefined){
-                    stats_by_date[fd_key] = {sum: 0, count: 0}; 
+                if (stats_by_date[fd_key] == undefined ){
+                    stats_by_date[fd_key] = []; 
                 }
-                stats_by_date[fd_key].sum += diff;
-                stats_by_date[fd_key].count++; 
+                stats_by_date[fd_key].push(diff)
             }
         },this);
-        console.log(dates_by_oid);
+        if (type == undefined){
+            this.rawChartData = stats_by_date;  
+        }
         
         var data = [];
-        Ext.each(categories, function(c){
+        var export_data ='';  
             for (var i=0; i<categories.length; i++){
                 var date_key = categories[i];
-                if (stats_by_date[date_key] && stats_by_date[date_key].count > 0){
-                    data[i] = stats_by_date[date_key].sum/stats_by_date[date_key].count;
-                } else {
-                    data[i] = 0;
+                data[i]=0;
+                var sdev = 0;
+                var count = 0;
+                if (stats_by_date[date_key] && stats_by_date[date_key].length > 0){
+                    count = stats_by_date[date_key].length;
+                    sdev = this._getStandardDeviation(stats_by_date[date_key]);
+                    data[i] = Math.max(Ext.Array.mean(stats_by_date[date_key]),0); 
+                } 
+                if (type == undefined){
+                    if (export_data.length == 0){
+                        export_data = "Date,Avg Cycle Time,Artifact Count,Standard Deviation\n";
+                    }
+                    export_data += Ext.String.format("{0},{1},{2},{3}\n",date_key,data[i], count, sdev);
                 }
             }
-        },this);
         
-        return [{
-             name: 'Average Cycle Time (',
-             data: data
-         }];
+        if (export_data.length > 0){
+            this.exportData = export_data;
+        }
+        
+        return {
+             name: this._getSeriesName(type),
+             data: data,
+             display: 'line'
+         };
     },
-    _getSeriesName: function(){
-        return Ext.String.format('Average Cycle Time ({0} to {1})',this.fromState, this.toState);   
-    },
-    _getSnapsByFinalDate: function(dates_by_oid) {
-        var oids_by_date = {};
-        Ext.Object.each(dates_by_oid, function(oid, snapshot) {
-            if ( snapshot._final_date ) {
-                var key_date = snapshot._final_date;
-                var short_date = snapshot._final_date.replace(/T.*$/,'');
-                if ( ! snaps_by_date[short_date] ) {
-                    snaps_by_date[short_date] = [];
-                }
-                snaps_by_date[short_date].push(snapshot);
-            }
+    _getStandardDeviation: function(vals){
+        var avg = Ext.Array.mean(vals);
+        
+        var sq_diff_array = vals.map(function(v){
+          var diff = v - avg;
+          var sq_diff = diff * diff;
+          return sq_diff;
         });
-        
-        return snaps_by_date;
-    },
-    _getCycleTimes: function(snaps_by_date){
-        var cycle_times_by_date = {};
-        
-        Ext.Object.each( snaps_by_date, function( key_date, snapshots ) {
-            var time_array = [];
-            Ext.Array.each(snapshots,function(snapshot){
-                var begin_time = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);
-                var end_time = Rally.util.DateTime.fromIsoString(snapshot._final_date);
-                var cycle_time = Rally.util.DateTime.getDifference(end_time,begin_time,'day');
-                time_array.push(cycle_time);
-            });
-            cycle_times_by_date[key_date] = Ext.Array.mean(time_array) || null;
-        });
-        
-        return cycle_times_by_date;
-    },
-    _orderAndFillDateHash: function(snaps_by_date){
-        // put existing keys in order:
-        var filled_snaps = {};
+        var avg_sq_diff = Ext.Array.mean(sq_diff_array);
        
-        var keys = Ext.Object.getKeys(snaps_by_date);
-        if ( keys.length > 0 ) {
-            var min = Ext.Array.min(keys);
-            var first_date = Rally.util.DateTime.fromIsoString(min);
-            var today = new Date();
-            
-            var check_date = first_date;
-            while( check_date < today ) {
-                var iso_date = Rally.util.DateTime.toIsoString(check_date).replace(/T.*$/,"");
-                filled_snaps[iso_date] = [];
-                if ( snaps_by_date[iso_date] ) {
-                    filled_snaps[iso_date] = snaps_by_date[iso_date];
-                }
-                check_date = Rally.util.DateTime.add(check_date,'day',1);
+        var sdev = Math.sqrt(avg_sq_diff);
+        return sdev;        
+    },
+    _getSeriesName: function(type){
+        var type_text = "Combined";
+        if (type) {
+            type_text = type;
+            if (type == 'HierarchicalRequirement'){
+                type_text = "User Story";
             }
         }
-        return filled_snaps;
-    }
+        return Ext.String.format(type_text);   
+    },
+//    _getSnapsByFinalDate: function(dates_by_oid) {
+//        var oids_by_date = {};
+//        Ext.Object.each(dates_by_oid, function(oid, snapshot) {
+//            if ( snapshot._final_date ) {
+//                var key_date = snapshot._final_date;
+//                var short_date = snapshot._final_date.replace(/T.*$/,'');
+//                if ( ! snaps_by_date[short_date] ) {
+//                    snaps_by_date[short_date] = [];
+//                }
+//                snaps_by_date[short_date].push(snapshot);
+//            }
+//        });
+//        
+//        return snaps_by_date;
+//    },
+//    _getCycleTimes: function(snaps_by_date){
+//        var cycle_times_by_date = {};
+//        
+//        Ext.Object.each( snaps_by_date, function( key_date, snapshots ) {
+//            var time_array = [];
+//            Ext.Array.each(snapshots,function(snapshot){
+//                var begin_time = Rally.util.DateTime.fromIsoString(snapshot._ValidFrom);
+//                var end_time = Rally.util.DateTime.fromIsoString(snapshot._final_date);
+//                var cycle_time = Rally.util.DateTime.getDifference(end_time,begin_time,'day');
+//                time_array.push(cycle_time);
+//            });
+//            cycle_times_by_date[key_date] = Ext.Array.mean(time_array) || null;
+//        });
+//        
+//        return cycle_times_by_date;
+//    },
+//    _orderAndFillDateHash: function(snaps_by_date){
+//        // put existing keys in order:
+//        var filled_snaps = {};
+//       
+//        var keys = Ext.Object.getKeys(snaps_by_date);
+//        if ( keys.length > 0 ) {
+//            var min = Ext.Array.min(keys);
+//            var first_date = Rally.util.DateTime.fromIsoString(min);
+//            var today = new Date();
+//            
+//            var check_date = first_date;
+//            while( check_date < today ) {
+//                var iso_date = Rally.util.DateTime.toIsoString(check_date).replace(/T.*$/,"");
+//                filled_snaps[iso_date] = [];
+//                if ( snaps_by_date[iso_date] ) {
+//                    filled_snaps[iso_date] = snaps_by_date[iso_date];
+//                }
+//                check_date = Rally.util.DateTime.add(check_date,'day',1);
+//            }
+//        }
+//        return filled_snaps;
+//    }
 });
--- anystate-cycle-time/src/javascript/app.js
+++ anystate-cycle-time/src/javascript/app.js
@@ -3,23 +3,90 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     logger: new Rally.technicalservices.Logger(),
     groupField: 'ScheduleState',
-    fromState: 'In-Progress',
-    toState: 'Accepted',
     items: [
-        {xtype:'container',itemId:'message_box',tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
+        {xtype:'container',itemId:'selector_box', layout: {type: 'hbox'}, padding:5, tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
         {xtype:'container',itemId:'display_box'},
         {xtype:'tsinfolink'}
     ],
     launch: function() {
-        this.down('#message_box').update(this.getContext().getUser());
         Ext.create('AnystateCycleCalculator',{});
-        this._createChart()
+        
+        this.down('#selector_box').add({
+            xtype: 'rallyfieldvaluecombobox',
+            itemId: 'cb-from-state',
+            model: 'HierarchicalRequirement',
+            field: 'ScheduleState',
+            fieldLabel:  'Start',
+            labelAlign: 'right',
+            margin: 10
+        });
+        this.down('#selector_box').add({
+            xtype: 'rallyfieldvaluecombobox',
+            itemId: 'cb-to-state',
+            model: 'HierarchicalRequirement',
+            field: 'ScheduleState',
+            fieldLabel:  'End',
+            labelAlign: 'right',
+            margin: 10
+        });
+        this.down('#selector_box').add({
+            xtype: 'rallycombobox',
+            itemId: 'cb-granularity',
+            store: ['Week','Month'],
+            fieldLabel:  'Granularity',
+            labelAlign: 'right',
+            margin: 10
+        });
+        this.down('#selector_box').add({
+            xtype: 'rallybutton',
+            itemId: 'btn-update',
+            text: 'Update',
+            scope: this,
+            margin: 10,
+            handler: this._createChart
+        });
+        this.down('#selector_box').add({
+            xtype: 'rallybutton',
+            itemId: 'btn-export',
+            text: 'Export',
+            scope: this,
+            margin: 10,
+            disabled: true, 
+            handler: this._exportData
+        });
         
     },
+    _getStartState: function(){
+        return this.down('#cb-from-state').getValue();
+    },
+    _getEndState: function(){
+        return this.down('#cb-to-state').getValue();
+    },
+    _getGranularity: function(){
+        return this.down('#cb-granularity').getValue();
+    },
+    _getTickInterval: function(granularity){
+        var tick_interval = 30;
+        if (granularity == 'Week') {tick_interval = 5;}
+        if (granularity == 'Month'){ tick_interval = 1;}
+        return tick_interval;  
+    },
+    _exportData: function(){
+        this.logger.log('_exportData');
+        var export_text = this.down('#rally-chart').calculator.exportData;
+        Rally.technicalservices.FileUtilities.saveTextAsFile(export_text);
+    },
     _createChart: function(){
+        this.logger.log('_createChart');
         if (this.down('#rally-chart')){
             this.down('#rally-chart').destroy();
         }
+        
+        var start_state = this._getStartState();
+        var end_state = this._getEndState();
+        var title_text = 'Average Cycle Time (Days) from ' + start_state + ' to ' + end_state;
+        var granularity = this._getGranularity();
+        var tick_interval = this._getTickInterval(granularity);  
         
         this.down('#display_box').add({
             xtype: 'rallychart',
@@ -28,9 +95,9 @@ Ext.define('CustomApp', {
             storeType: 'Rally.data.lookback.SnapshotStore',
             storeConfig: this._getStoreConfig(),
             calculatorConfig: {
-                initial_state: this.fromState,
-                final_state: this.toState,
-                group_field: this.groupField
+                startState: this._getStartState(),
+                endState: this._getEndState(),
+                granularity: granularity
             },
             chartConfig: {
                 chart: {
@@ -38,39 +105,52 @@ Ext.define('CustomApp', {
                     type: 'line'
                 },
                 title: {
-                    text: 'Defined to In-Progress'
+                    text: title_text
                 },
                 xAxis: {
-                    tickmarkPlacement: 'on',
-                    tickInterval: 30,
+//                    tickmarkPlacement: 'on',
+                    tickInterval: tick_interval,
                     title: {
-                        text: 'Date Entered Final State'
+                        text: 'Date Entered ' + end_state
                     }
                 },
                 yAxis: [
                     {
                         title: {
-                            text: 'Days'
+                            text: granularity
                         }
                     }
-                ]
+                ],
+                plotOptions: {
+                    series: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+            },
+            listeners: {
+                scope: this,
+                readyToRender: function(chart){
+                    this.down('#btn-export').setDisabled(false);
+                }
             }
         });
 
     },
     _getStoreConfig: function(){
-        var previous_value_group_field = '_PreviousValues.' + this.groupField;  
-        
+       var start_state = this._getStartState();
+       var end_state = this._getEndState(); 
         return {
             find: {
-                 Children: null,
-                 _ProjectHierarchy: this.getContext().getProject().ObjectID,
-                 _TypeHierarchy: {$in: ['HierarchicalRequirement']},
-                 ScheduleState: {$in: [this.fromState, this.toState]}
-     //            _ValidFrom: Rally.util.DateTime.add(new Date(),'day',-120)
+                 "Children": null,
+                 "_ProjectHierarchy": this.getContext().getProject().ObjectID,
+                 "_TypeHierarchy": {$in: ['HierarchicalRequirement','Defect']},
+                 "ScheduleState": {$in: [start_state, end_state]},
+                 "_PreviousValues.ScheduleState": {$exists: true}
             },
-            fetch: ['ObjectID','ScheduleState','_ValidFrom','_ValidTo','_PreviousValues.ScheduleState'],
-            hydrate: ['ScheduleState',"_PreviousValues.ScheduleState"],
+            fetch: ['ObjectID','ScheduleState','_ValidFrom','_ValidTo','_PreviousValues.ScheduleState','_SnapshotNumber','_TypeHierarchy'],
+            hydrate: ['ScheduleState',"_PreviousValues.ScheduleState", '_TypeHierarchy'],
             compress: true,
             sort: {
                 _ValidFrom: 1
